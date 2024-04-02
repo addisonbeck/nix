ca66319735fedde96be792e85cbe0d54073d279e
Addison Beck <hello@addisonbeck.com>
2024-04-02 14:43:09 -0500

    .

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
       # FIXME replace with your username@hostname
       "me@dev" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
+        extraSpecialArgs = {inherit inputs outputs agenix;};
         # > Our main home-manager configuration file <
         modules = [./home-manager/home.nix];
       };
