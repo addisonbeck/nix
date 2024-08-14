9db54aa4bea3b8cbfff215981eaecc5b0bd6786c
Addison Beck <github@addisonbeck.com>
2024-08-14 17:16:36 -0400

    Relocate raycast service declaration

--- system/with/user/with/program/raycast.nix
+++ system/with/user/with/program/raycast.nix
@@ -1,1 +1,13 @@
-{ pkgs, ... }: { home.packages = [ pkgs.raycast ]; }
+{ pkgs, ... }: {
+  home.packages = [ pkgs.raycast ];
+
+  launchd.agents.raycast = {
+    enable = true;
+    config = {
+      ProgramArguments =
+        [ "${pkgs.raycast}/Applications/Raycast.app/Contents/MacOS/Raycast" ];
+      KeepAlive = true;
+      RunAtLoad = true;
+    };
+  };
+}
