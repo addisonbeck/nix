cc4e7f876d0571c4f5676306cb0d502ce965191b
Addison Beck <github@addisonbeck.com>
2024-08-16 13:35:27 -0400

    Update me.nix

--- system/with/user/me.nix
+++ system/with/user/me.nix
@@ -6,18 +6,18 @@
 
   users.knownUsers = [ "me" ];
 
-  users.users.me = {
-    shell = pkgs.fish;
-  } // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
-    uid = 503;
-    name = "me";
-    home = "/Users/me";
-    createHome = true;
-  } // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-    users.users.me.isNormalUser = true;
-    users.users.me.initialPassword = "me";
-    users.users.me.extraGroups = [ "wheel" "docker" ];
-  };
+  #users.users.me = {
+    #shell = pkgs.fish;
+  #} // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
+    #uid = 503;
+    #name = "me";
+    #home = "/Users/me";
+    #createHome = true;
+  #} // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+    #users.users.me.isNormalUser = true;
+    #users.users.me.initialPassword = "me";
+    #users.users.me.extraGroups = [ "wheel" "docker" ];
+  #};
 
   home-manager.users.me = {
     imports = [
