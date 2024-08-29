0693aecb8b04dd2b96d86541d2057a4d59356dc1
Addison Beck <github@addisonbeck.com>
2024-08-28 20:05:00 -0400

    .

--- system/with/user/with/program/lazygit.nix
+++ system/with/user/with/program/lazygit.nix
@@ -1,2 +1,18 @@
-{ ... }: { programs.lazygit.enable = true; }
+{ pkgs, ... }: {
+  programs.lazygit.enable = true;
+  #programs.lazygit.settings.pager = "${pkgs.delta}/bin/delta --dark --paging=never";
+  programs.lazygit.settings = {
+    git.paging.colorArg = "always";
+    git.paging.pager = "${pkgs.delta}/bin/delta --dark --paging=never";
+    gui.portraitMode = "auto";
+    customCommands = [{
+      key = "O";
+      command = "nvim -c ':Octo pr create draft'";
+      context = "localBranches";
+      loadingText = "Loading Octo";
+      description = "Open pull request with Octo";
+      subprocess = true;
+    }];
+  };
+}
 
