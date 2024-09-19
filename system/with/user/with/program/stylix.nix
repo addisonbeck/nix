e702d30639fc937bf47b17a4d6ff9b960ca65259
Addison Beck <github@addisonbeck.com>
2024-09-19 15:04:50 -0400

    Swap theme

--- system/with/user/with/program/stylix.nix
+++ system/with/user/with/program/stylix.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   stylix.enable = true;
   stylix.image = ../wallpaper/empty.png;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-light-hard.yaml";
 
   stylix.fonts = {
     serif = {
