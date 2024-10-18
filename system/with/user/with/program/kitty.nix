73d81905529c9e25ca71d1f7aa316ba88625b84c
Addison Beck <github@addisonbeck.com>
2024-10-17 22:32:47 -0400

    Adjust padding of the terminal window

--- system/with/user/with/program/kitty.nix
+++ system/with/user/with/program/kitty.nix
@@ -6,7 +6,7 @@
   #programs.kitty.settings.background_opacity = "0.8";
   programs.kitty.settings.hide_window_decorations = "no";
   programs.kitty.settings.confirm_os_window_close = "0";
-  programs.kitty.settings.window_padding_width = "0 30";
+  programs.kitty.settings.window_padding_width = "5 30";
   programs.kitty.settings.undercurl_style = "thin-sparse";
   programs.kitty.settings.cursor_blink_interval = "0.5";
   programs.kitty.settings.cursor_stop_blinking_after = "0";
