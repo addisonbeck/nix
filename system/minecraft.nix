db089aee7cb35d76af9c08dff98d66f1676cd2db
Addison Beck <github@addisonbeck.com>
2024-10-20 20:16:42 +0000

    Add some random jvmOpts found online to minecraft

--- system/minecraft.nix
+++ system/minecraft.nix
@@ -37,6 +37,8 @@
 	  # user = "steve";
 	  # group = "steve";
 	  servers.bonesfamily = {
+
+	    jvmOpts = ''-Xmx6G -Xms6G -XX:+UseG1GC -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M'';
 	    enable = true;
 	    enableReload = false;
 	    package = inputs.nix-minecraft.legacyPackages.x86_64-linux.fabricServers.fabric-1_21_1;
