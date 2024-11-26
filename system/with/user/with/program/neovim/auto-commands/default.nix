63202c2013f95949ec280d808228e6ac1c26650b
Addison Beck <github@addisonbeck.com>
2024-11-26 11:27:42 -0500

    Turn of the extmark stuff in markdown for now.

--- system/with/user/with/program/neovim/auto-commands/default.nix
+++ system/with/user/with/program/neovim/auto-commands/default.nix
@@ -33,62 +33,62 @@
         '';
       };
     }
-    {
-      event = [
-        "BufEnter"
-        "FileChangedShellPost"
-        "Syntax"
-        "TextChanged"
-        "TextChangedI"
-        "InsertLeave"
-        "WinScrolled"
-      ];
-      pattern = [
-        "*.markdown"
-        "*.md"
-      ];
-      callback = {
-        __raw = ''
-          function()
-            local function get_code_blocks()
-              local parser = vim.treesitter.get_parser(0)
-              local tree = parser:parse()[1]
-              local root = tree:root()
-
-              local function find_code_blocks(node)
-                local result = {}
-                local function traverse(node)
-                  if node:type() == 'fenced_code_block' then
-                    local start_row, start_col, end_row, end_col = node:range()
-                    table.insert(result, {start_row, start_col, end_row, end_col})
-                  end
-                  for child in node:iter_children() do
-                    traverse(child)
-                  end
-                end
-                traverse(node)
-                return result
-              end
-
-              return find_code_blocks(root)
-            end
-
-            local code_blocks = get_code_blocks()
-            local namespace = vim.api.nvim_create_namespace('ab_md_code_blocks')
-            vim.api.nvim_buf_clear_namespace(0, namespace, 0, -1)
-            for _, node in ipairs(code_blocks) do
-              local start_row, start_col, end_row, end_col = unpack(node)
-              vim.api.nvim_buf_set_extmark(0, namespace, start_row, start_col, {
-                end_row = end_row,
-                end_col = end_col,
-                hl_group = "CodeFence",
-                hl_eol = true,
-              })
-            end
-          end
-        '';
-      };
-    }
+    # {
+    #   event = [
+    #     "BufEnter"
+    #     "FileChangedShellPost"
+    #     "Syntax"
+    #     "TextChanged"
+    #     "TextChangedI"
+    #     "InsertLeave"
+    #     "WinScrolled"
+    #   ];
+    #   pattern = [
+    #     "*.markdown"
+    #     "*.md"
+    #   ];
+    #   callback = {
+    #     __raw = ''
+    #       function()
+    #         local function get_code_blocks()
+    #           local parser = vim.treesitter.get_parser(0)
+    #           local tree = parser:parse()[1]
+    #           local root = tree:root()
+    #
+    #           local function find_code_blocks(node)
+    #             local result = {}
+    #             local function traverse(node)
+    #               if node:type() == 'fenced_code_block' then
+    #                 local start_row, start_col, end_row, end_col = node:range()
+    #                 table.insert(result, {start_row, start_col, end_row, end_col})
+    #               end
+    #               for child in node:iter_children() do
+    #                 traverse(child)
+    #               end
+    #             end
+    #             traverse(node)
+    #             return result
+    #           end
+    #
+    #           return find_code_blocks(root)
+    #         end
+    #
+    #         local code_blocks = get_code_blocks()
+    #         local namespace = vim.api.nvim_create_namespace('ab_md_code_blocks')
+    #         vim.api.nvim_buf_clear_namespace(0, namespace, 0, -1)
+    #         for _, node in ipairs(code_blocks) do
+    #           local start_row, start_col, end_row, end_col = unpack(node)
+    #           vim.api.nvim_buf_set_extmark(0, namespace, start_row, start_col, {
+    #             end_row = end_row,
+    #             end_col = end_col,
+    #             hl_group = "CodeFence",
+    #             hl_eol = true,
+    #           })
+    #         end
+    #       end
+    #     '';
+    #   };
+    # }
     {
       event = ["BufWinEnter"];
       # TODO: Fine a real syntax highlighting solution for haxe
