6f31409f409ec1fda5db9457fe4df2c458b2b6d7
Addison Beck <github@addisonbeck.com>
2025-01-31 11:31:55 -0500

    Freshrss

--- system/with/user/with/secret/freshrss.nix
+++ system/with/user/with/secret/freshrss.nix
@@ -1,8 +1,5 @@
 {
-  config,
   ...
 }: {
-  age.secretsDir = "${config.home.homeDirectory}/.secrets";
-  age.identityPaths = ["${config.home.homeDirectory}/.ssh/me"];
   age.secrets.freshrss.file = ./freshrss.age;
 }
