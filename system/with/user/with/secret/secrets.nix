65976ff0947d86a725b9837263e4ceea6812da8b
Addison Beck <github@addisonbeck.com>
2025-01-31 15:43:26 -0500

    Rekey

--- system/with/user/with/secret/secrets.nix
+++ system/with/user/with/secret/secrets.nix
@@ -1,6 +1,6 @@
 let
   me_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJSLY/c9uffjNA0T8o8CjrAI7DdvxNyp0SNBeLjQ4pH";
-  rss_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE47xEbEYaViDJnprkHSFcFuwMHBdvHGFxcIw4+IsTYI root@rss.addisonbeck.dev";
+  rss_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINCHL5ld7YdBz5uwVhyXmKFR8hcvPkvlcu3leSB/7nHt root@rss.addisonbeck.dev";
 in {
   "github.age".publicKeys = [me_key];
   "email.age".publicKeys = [me_key];
