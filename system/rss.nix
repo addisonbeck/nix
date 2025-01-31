7aaa5f2b6d34f9f76171b9ea017078f20e59c08d
Addison Beck <github@addisonbeck.com>
2025-01-31 16:46:41 -0500

    freshrss

--- system/rss.nix
+++ system/rss.nix
@@ -40,23 +40,27 @@
       "rss.addisonbeck.dev" = {
         forceSSL = true;
         enableACME = true;
-        locations."~ ^/rss.addisonbeck.dev/.+?\.php(/.*)?$"={
-            root = "${pkgs.freshrss}/p";
-            extraConfig = ''
-              fastcgi_pass unix:${config.services.phpfpm.pools.freshrss.socket};
-              fastcgi_split_path_info ^/l_path}(/.+\.php)(/.*)?$;
-              set $path_info $fastcgi_path_info;
-              fastcgi_param PATH_INFO $path_info;
-              fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-              include ${pkgs.nginx}/conf/fastcgi_params;
-              include ${pkgs.nginx}/conf/fastcgi.conf;
-         '';
-          locations."~ ^/rss.addisonbeck.dev(?!.*\.php)(/.*)?$" = {
-            root = "${pkgs.freshrss}/p";
-            tryFiles = "$1 /rss.addisonbeck.dev$1/index.php$is_args$args";
+          root = "${pkgs.freshrss}/p";
+
+          # php files handling
+          # this regex is mandatory because of the API
+          locations."~ ^.+?\\.php(/.*)?$".extraConfig = ''
+            fastcgi_pass unix:${config.services.phpfpm.pools."freshrss".socket};
+            fastcgi_split_path_info ^(.+\.php)(/.*)$;
+            # By default, the variable PATH_INFO is not set under PHP-FPM
+            # But FreshRSS API greader.php need it. If you have a “Bad Request” error, double check this var!
+            # NOTE: the separate $path_info variable is required. For more details, see:
+            # https://trac.nginx.org/nginx/ticket/321
+            set $path_info $fastcgi_path_info;
+            fastcgi_param PATH_INFO $path_info;
+            include ${pkgs.nginx}/conf/fastcgi_params;
+            include ${pkgs.nginx}/conf/fastcgi.conf;
+          '';
+
+          locations."/" = {
+            tryFiles = "$uri $uri/ index.php";
             index = "index.php index.html index.htm";
           };
-        };
       };
     };
   };
