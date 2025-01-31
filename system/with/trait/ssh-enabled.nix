fae4cc95b86a141864dd592e832cf03a2006abd7
Addison Beck <github@addisonbeck.com>
2025-01-31 07:21:05 -0500

    Freshrss

--- system/with/trait/ssh-enabled.nix
+++ system/with/trait/ssh-enabled.nix
@@ -2,6 +2,5 @@
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "no";
   services.openssh.settings.PasswordAuthentication = false;
-  services.openssh.firewall.KbdInteractiveAuthentication = false;  
   networking.firewall.allowedTCPPorts = [22];
 }
