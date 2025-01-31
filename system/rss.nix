45a272d380028989853e5273841c46231dcea3e6
Addison Beck <github@addisonbeck.com>
2025-01-31 13:27:23 -0500

    Freshrss

--- system/rss.nix
+++ system/rss.nix
@@ -49,7 +49,19 @@ in
     virtualHosts."rss.addisonbeck.dev" = {
         enableACME = true;
         forceSSL = true;
+        extraConfig = ''
+          fastcgi_buffers 16 16k;
+          fastcgi_buffer_size 32k;
+        '';
+        locations."~ \\.php$".extraConfig = ''
+          fastcgi_pass unix:/run/phpfpm/freshrss;
+          fastcgi_index index.php;
+          include ${pkgs.nginx}/conf/fastcgi_params;
+          fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+          fastcgi_param PATH_INFO $fastcgi_path_info;
+        '';
     };
+
   };
   networking.firewall = {
     enable = true;
@@ -57,15 +69,15 @@ in
   };
 
   services.phpfpm.pools.freshrss = {
-    user = "freshrss";
+    user = config.services.freshrss.user;
     settings = {
+      "listen" = "/run/phpfpm/freshrss";
+      "listen.owner" = config.services.nginx.user;
       "pm" = "dynamic";
       "pm.max_children" = 32;
       "pm.start_servers" = 2;
       "pm.min_spare_servers" = 2;
       "pm.max_spare_servers" = 5;
-      "php_admin_value[session.cookie_secure]" = "On";
-      "php_admin_value[session.cookie_httponly]" = "On";
     };
   };
 
