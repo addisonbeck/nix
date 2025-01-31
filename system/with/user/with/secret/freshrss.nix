3f78f235fbe6158361cc43af40dcff78c41ceac6
Addison Beck <github@addisonbeck.com>
2025-01-31 15:39:27 -0500

    Freshrss

--- system/with/user/with/secret/freshrss.nix
+++ system/with/user/with/secret/freshrss.nix
@@ -5,7 +5,6 @@
     age.secrets.freshrss = {
       file = ./freshrss.age;  # Path to your encrypted secret
       owner = config.services.freshrss.user;
-      group = config.services.freshrss.group;
       mode = "0400";
     };
 }
