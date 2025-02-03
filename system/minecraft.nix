723052cb999c444f8396da94c10d497d24440929
Addison Beck <github@addisonbeck.com>
2025-02-03 01:05:44 +0000

    .

--- system/minecraft.nix
+++ system/minecraft.nix
@@ -33,29 +33,29 @@
     }
     inputs.nix-minecraft.nixosModules.minecraft-servers
     {
-      services.minecraft-servers = {
-        enable = true;
-        eula = true;
-        #        managementSystem.tmux = {
-        #   enable = true;
-        #   socketPath = name: "/home/steve/sessions/${name}.sock";
-        # };
-        # dataDir = "/home/steve/servers/";
-        # user = "steve";
-        # group = "steve";
-        servers.bonesfamily = {
-          jvmOpts = ''-Xmx6G -Xms6G -XX:+UseG1GC -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M'';
-          enable = true;
-          enableReload = false;
-          package = inputs.nix-minecraft.legacyPackages.x86_64-linux.fabricServers.fabric-1_21_4;
-          openFirewall = true;
-          autoStart = true;
-          serverProperties = {
-            default-player-permission-level = "visitor";
-            motd = "Bones bones bones";
-            difficulty = "easy";
-            gamemode = "survival";
-          };
+	services.minecraft-servers = {
+	  enable = true;
+	  eula = true;
+	  managementSystem.systemd-socket = {
+	    enable = true;
+	  };
+	  # dataDir = "/home/steve/servers/";
+	  # user = "steve";
+	  # group = "steve";
+	  servers.bonesfamily = {
+	    #jvmOpts = ''-Xmx6G -Xms6G -XX:+UseG1GC -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M'';
+            jvmOpts = ''-Xms6144M -Xmx6144M -XX:+UseZGC'';
+	    enable = true;
+	    enableReload = false;
+            package = inputs.nix-minecraft.legacyPackages.x86_64-linux.fabricServers.fabric-1_21_4;
+	    openFirewall = true;
+	    autoStart = true;
+	    serverProperties = {
+	     default-player-permission-level = "visitor";
+	     motd = "Bones bones bones";
+	     difficulty = "easy";
+	     gamemode = "survival";
+	    };
           files = {
             "config/Geyser-Fabric/config.yml".value = {
               remote."auth-type" = "floodgate";
