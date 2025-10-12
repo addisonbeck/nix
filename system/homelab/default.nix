bde2d6e22ec2a33550ba078cda9835b6798f8848
addisonbeck <github@addisonbeck.com>
2025-10-12 14:47:19 -0400

    turn off a bunch of stuff I'm not really using

--- system/homelab/default.nix
+++ system/homelab/default.nix
@@ -6,15 +6,15 @@
     ./modules/system-boot.nix
     ./modules/filesystems.nix
     ./modules/tailscale.nix
-    ./modules/prometheus.nix
-    ./modules/grafana.nix
+    #./modules/prometheus.nix
+    #./modules/grafana.nix
     ./modules/adguardhome.nix
     ./modules/freshrss.nix
-    ./modules/vaultwarden.nix
+    #./modules/vaultwarden.nix
     ../modules/automated-emailing
     ../modules/secrets/adguard-env-file.nix
-    ../modules/secrets/vaultwarden-env-file.nix
-    ../modules/secrets/grafana.nix
+    #../modules/secrets/vaultwarden-env-file.nix
+    #../modules/secrets/grafana.nix
     ../modules/secrets/automated-emailing.nix
     ../modules/secrets/freshrss.nix
     ./modules/calibre/default.nix
