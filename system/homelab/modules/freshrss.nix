4ad264974625c7265b29d667a687489644c6cdc1
addisonbeck <github@addisonbeck.com>
2025-10-12 11:22:46 -0400

    still trying to fix freshrss

--- system/homelab/modules/freshrss.nix
+++ system/homelab/modules/freshrss.nix
@@ -18,19 +18,19 @@
         index = "index.php";
         tryFiles = "$uri $uri/ /index.php$is_args$args";  # Changed this line
         extraConfig = ''
-          # php files handling
-          # this regex is mandatory because of the API
           location ~ ^.+?\.php(/.*)?$ {
-            fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+            fastcgi_pass unix:${config.services.phpfpm.pools."freshrss".socket};
             fastcgi_split_path_info ^(.+\.php)(/.*)$;
-            # By default, the variable PATH_INFO is not set under PHP-FPM
-            # But FreshRSS APIs greader.php and misc.php need it. If you have a “Bad Request” error, double check this var!
-            # NOTE: the separate $path_info variable is required. For more details, see:
-            # https://trac.nginx.org/nginx/ticket/321
+            fastcgi_param PATH_INFO $fastcgi_path_info;
             set $path_info $fastcgi_path_info;
             fastcgi_param PATH_INFO $path_info;
-            include fastcgi_params;
+
+            # include fastcgi_params;
+            include ${pkgs.nginx}/conf/fastcgi_params;
+
+            # fastcgi_param SCRIPT_FILENAME $request_filename;
             fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+          }
         '';
     };
     "~ ^/rss/(.+\\.php)(/.*)?$" = {
