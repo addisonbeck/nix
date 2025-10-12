6b5c8681cdb76f4ae8665ce1620b12777e8bf955
addisonbeck <github@addisonbeck.com>
2025-10-11 23:42:21 -0400

    checkpoint

--- system/homelab/modules/freshrss.nix
+++ system/homelab/modules/freshrss.nix
@@ -16,18 +16,26 @@
       "/rss/" = {
         alias = "${pkgs.freshrss}/p/";
         index = "index.php";
-        tryFiles = "$uri $uri/ /rss/index.php$is_args$args";
-      };
-      "~ ^/rss/(.+\\.php)(/.*)?$" = {
+        tryFiles = "$uri $uri/ /index.php$is_args$args";  # Changed this line
         extraConfig = ''
-          fastcgi_pass unix:${config.services.phpfpm.pools."freshrss".socket};
-          include ${pkgs.nginx}/conf/fastcgi_params;
-          include ${pkgs.nginx}/conf/fastcgi.conf;
-          fastcgi_param SCRIPT_FILENAME ${pkgs.freshrss}/p/$1;
-          fastcgi_split_path_info ^/rss/(.+\\.php)(/.*)?$;
-          fastcgi_param PATH_INFO $fastcgi_path_info;
+          location ~ \.php$ {
+            fastcgi_pass unix:${config.services.phpfpm.pools."freshrss".socket};
+            include ${pkgs.nginx}/conf/fastcgi_params;
+            fastcgi_param SCRIPT_FILENAME $request_filename;
+            fastcgi_param PATH_INFO $fastcgi_path_info;
+          }
         '';
-      };
+    };
+    "~ ^/rss/(.+\\.php)(/.*)?$" = {
+      extraConfig = ''
+        fastcgi_pass unix:${config.services.phpfpm.pools."freshrss".socket};
+        include ${pkgs.nginx}/conf/fastcgi_params;
+        include ${pkgs.nginx}/conf/fastcgi.conf;
+        fastcgi_param SCRIPT_FILENAME ${pkgs.freshrss}/p/$1;
+        fastcgi_split_path_info ^/rss/(.+\\.php)(/.*)?$;
+        fastcgi_param PATH_INFO $fastcgi_path_info;
+      '';
     };
   };
+};
 }
