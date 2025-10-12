229c192a6b0c52738054ecb8417b4194f889b623
addisonbeck <github@addisonbeck.com>
2025-10-12 14:37:46 -0400

    putting things back the way they were

--- system/homelab/modules/freshrss.nix
+++ system/homelab/modules/freshrss.nix
@@ -12,37 +12,22 @@
     extensions = [pkgs.freshrss-extensions.youtube];
   };
   services.nginx.virtualHosts = {
-    "homelab".locations = {
+    "homelab-server".locations = {
       "/rss/" = {
         alias = "${pkgs.freshrss}/p/";
         index = "index.php";
-        tryFiles = "$uri $uri/ /index.php$is_args$args";  # Changed this line
+        tryFiles = "$uri $uri/ /rss/index.php$is_args$args";
+      };
+      "~ ^/rss/(.+\\.php)(/.*)?$" = {
         extraConfig = ''
-          location ~ ^.+?\.php(/.*)?$ {
-            fastcgi_pass unix:${config.services.phpfpm.pools."freshrss".socket};
-            fastcgi_split_path_info ^(.+\.php)(/.*)$;
-            fastcgi_param PATH_INFO $fastcgi_path_info;
-            set $path_info $fastcgi_path_info;
-            fastcgi_param PATH_INFO $path_info;
-
-            # include fastcgi_params;
-            include ${pkgs.nginx}/conf/fastcgi_params;
-
-            # fastcgi_param SCRIPT_FILENAME $request_filename;
-            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-          }
+          fastcgi_pass unix:${config.services.phpfpm.pools."freshrss".socket};
+          include ${pkgs.nginx}/conf/fastcgi_params;
+          include ${pkgs.nginx}/conf/fastcgi.conf;
+          fastcgi_param SCRIPT_FILENAME ${pkgs.freshrss}/p/$1;
+          fastcgi_split_path_info ^/rss/(.+\\.php)(/.*)?$;
+          fastcgi_param PATH_INFO $fastcgi_path_info;
         '';
-    };
-    "~ ^/rss/(.+\\.php)(/.*)?$" = {
-      extraConfig = ''
-        fastcgi_pass unix:${config.services.phpfpm.pools."freshrss".socket};
-        include ${pkgs.nginx}/conf/fastcgi_params;
-        include ${pkgs.nginx}/conf/fastcgi.conf;
-        fastcgi_param SCRIPT_FILENAME ${pkgs.freshrss}/p/$1;
-        fastcgi_split_path_info ^/rss/(.+\\.php)(/.*)?$;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
-      '';
+      };
     };
   };
-};
 }
